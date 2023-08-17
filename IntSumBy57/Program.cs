a3c1983c7135de9c5b1bad72031b2c15070cac78
Grant Magella <Bitman2003@gmail.com>
2023-08-17 16:01:40 -0400

    Add project files.

---
+++ IntSumBy57/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float sum = 0;
+        for (int idx = 0; idx <= 50; idx++)
+        {
+            if (idx % 5 == 0||idx % 7 == 0)
+            {
+                sum += idx;
+            }
+        }
+        Console.WriteLine($"Sum:" + sum);
+        float avg = sum / 50;
+        Console.WriteLine($"Average: " + avg);
+    }
+}
